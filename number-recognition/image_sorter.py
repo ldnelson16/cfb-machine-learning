8cea78a571b240529272d01b9a6bf1a1fa51d7e0
ldnelson16 <ldnelson16@gmail.com>
2023-11-26 17:23:22 -0500

    Sorting images into folder

--- number-recognition/image_sorter.py
+++ number-recognition/image_sorter.py
@@ -1,20 +1,36 @@
 ### IMPORTS
-import os, shutil, glob
+import os, shutil, glob, csv
+import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
-def split_data(path_to_images,path_training_images,path_val_images,path_test_images, split_val=0.1, split_test=0.1):
-  folders = os.listdir(path_to_images)
+def split_data(parent_folder, csv_path, split_val=0.1, split_test=0.1):
+  df = pd.read_csv(csv_path, index_col='filename')
+  images_paths = glob.glob(os.path.join(parent_folder, 'number-recognition', 'train_player_numbers', '*.png'))
 
-  for folder in folders:
+  x_train, x_temp = train_test_split(images_paths, test_size=split_val + split_test, random_state=1254)
+  x_val, x_test = train_test_split(x_temp, test_size=split_test / (split_test + split_val), random_state=1354)
 
-    full_path = os.path.join(path_to_images,folder)
-    images_paths = glob.glob(os.path.join(full_path, '*.png'))
-    x_train, x_temp = train_test_split(images_paths, test_size=split_val+split_test, random_state=1254)
-    x_val, x_test = train_test_split(x_temp, test_size=split_test/(split_test+split_val), random_state=1354)
+  path_training_images = os.path.join(parent_folder, 'number-recognition', 'train')
+  path_val_images = os.path.join(parent_folder, 'number-recognition', 'val')
+  path_test_images = os.path.join(parent_folder, 'number-recognition', 'test')
 
-    for (set,path) in [(x_train,path_training_images),(x_val,path_val_images),(x_test,path_test_images)]:
-      for x in set:
-        path_to_folder = os.path.join(path, folder)
+  for path in [path_training_images, path_val_images, path_test_images]:
+    if not os.path.isdir(path):
+      os.makedirs(path)
+
+  for (set_paths, path) in [(x_train, path_training_images), (x_val, path_val_images), (x_test, path_test_images)]:
+    for image_path in set_paths:
+      filename = os.path.basename(image_path)
+      label = df.loc[filename, "label"]
+      if isinstance(label,np.int64):
+        path_to_folder = os.path.join(path, str(label))
         if not os.path.isdir(path_to_folder):
-          os.makedirs(path_to_folder)
-        shutil.copy(x,path_to_folder)
+            os.makedirs(path_to_folder)
+
+        shutil.copy(image_path, path_to_folder)
+
+split_data(
+  parent_folder=os.getcwd(),
+  csv_path="number-recognition/train_player_numbers.csv"
+)
