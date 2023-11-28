b175ee43a4f63bd4ef28ea80325296f070fe9984
ldnelson16 <ldnelson16@gmail.com>
2023-11-28 07:20:36 -0500

    Sorting images into numberized folders

--- number-recognition/image_sorter.py
+++ number-recognition/image_sorter.py
@@ -1,7 +1,8 @@
 ### IMPORTS
-import os, shutil, glob, csv
+import os, shutil, glob
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 def split_data(parent_folder, csv_path, split_val=0.1, split_test=0.1):
@@ -30,7 +31,45 @@ def split_data(parent_folder, csv_path, split_val=0.1, split_test=0.1):
 
         shutil.copy(image_path, path_to_folder)
 
-split_data(
-  parent_folder=os.getcwd(),
-  csv_path="number-recognition/train_player_numbers.csv"
-)
+def data_generators(batch_size,train_data_path,val_data_path,test_data_path):
+  preprocessor = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    #rotation_range=15,     
+    #width_shift_range=0.1,  
+    #height_shift_range=0.1, 
+    #shear_range=0.1,   
+    zoom_range=0.1
+  )
+  test_preprocessor = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale = 1/255.
+  )
+  train_generator = preprocessor.flow_from_directory(
+    train_data_path,
+    class_mode="categorical",
+    target_size=(64,64),
+    color_mode="rgb",
+    shuffle=True,
+    batch_size=batch_size
+  )  
+  val_generator = test_preprocessor.flow_from_directory(
+    val_data_path,
+    class_mode="categorical",
+    target_size=(64,64),
+    color_mode="rgb",
+    shuffle=True,
+    batch_size=batch_size
+  )  
+  test_generator = test_preprocessor.flow_from_directory(
+    test_data_path,
+    class_mode="categorical",
+    target_size=(64,64),
+    color_mode="rgb",
+    shuffle=True,
+    batch_size=batch_size
+  )
+  return train_generator,val_generator,test_generator 
+
+#split_data(
+  #parent_folder=os.getcwd(),
+  #csv_path="number-recognition/train_player_numbers.csv"
+#)
